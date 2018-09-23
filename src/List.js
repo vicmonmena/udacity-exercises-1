572283fe147e3c929d4668af113f4651974a0a16
Vicente Montaño Mena <vicmonmena@gmail.com>
2018-09-23 15:40:00 +0000

    First commit

---
+++ src/List.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+// Example: Jane Cruz's favorite movie is Planet Earth 1.
+
+class List extends Component {
+  
+  render() {
+    const { profiles, users, movies} = this.props;
+
+    return(
+		<ul> 
+      	{
+			profiles.map(profile => 
+          		(
+				<li key={profile.id}>
+                  <div>
+      				
+      					{
+                          users.find(user => {
+                          	// == comparator because user.id is number and profile.userID is string :_(
+                         	return user.id === Number(profile.userID)
+  							}).name
+						}
+						`'s favorite movie is`
+						{movies.find(movie => (movie.id === Number(profile.favoriteMovieID))).name}
+					
+                  </div>
+				</li> 
+          		)
+			)
+		}
+		</ul>
+    )
+  }
+}
+
+export default List;
